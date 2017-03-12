422c960a11dfc9239c996def412dc32e2608beb4
william chou <w.chou06@gmail.com>
2017-03-11 23:22:12 -0500

    First commit

---
+++ ex1.rb
@@ -0,0 +1,26 @@
+# class ClassName
+#   def initialize
+    
+#   end
+# end
+
+module Speak
+  def speak(sound)
+    puts "#{sound}"
+  end
+end
+
+class GoodDog
+  include Speak
+end
+
+class HumanBeing
+  include Speak
+end
+
+sparky = GoodDog.new
+sparky.speak("Arf!")        # => Arf!
+puts sparky
+bob = HumanBeing.new
+bob.speak("Hello!")         # => Hello!
+puts bob
