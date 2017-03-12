e5aca6c2791500e6004f719426be7403dfc5ff65
william chou <w.chou06@gmail.com>
2017-03-11 23:59:42 -0500

    refactored code following roby best practices

--- ex2.rb
+++ ex2.rb
@@ -1,37 +1,54 @@
 # good_dog.rb
 
 class GoodDog
-  def initialize(name)
+  #use attribute accessor for simpler get and sets.
+  attr_accessor :name, :height, :weight
+  #attr_reader
+  #attr_writer
+  #All attr take symbols. can call multiple by seperating variables with commas.
+
+  def initialize(n, h, w)
     #this is an instance variable. It holds the state of the object.
-    @name = name
+    @name = n
+    @height = h
+    @weight = w
   end
 
+  #need self. so the methods know we are referring to this object .
+  def change_info(n, h, w)
+    self.height = h
+    self.name = n
+    self.weight = w
+  end
 
 # this is an instance method
+# removed the @ and instead called the getter method.
   def speak 
-    "#{@name} says Arf!"
+    "#{name} says Arf!"
   end
 
-  def name                  # This was renamed from "get_name"
-    @name
+  def info
+    "#{self.name} weighs #{self.weight} and is #{self.height} tall."
   end
 
-  def name=(n)              # This was renamed from "set_name="
-    @name = n
-  end
 end
 
-sparky = GoodDog.new("Sparky")       # => "This object was initialized!"
-# puts sparky
-puts sparky.speak
-fido = GoodDog.new("Fido")
-# puts fido
-puts fido.speak
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info      # => Sparky weighs 10 lbs and is 12 inches tall.
 
-puts sparky.name
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info      # => Spartacus weighs 45 lbs and is 24 inches tall.
+# sparky = GoodDog.new("Sparky")       # => "This object was initialized!"
+# # puts sparky
+# puts sparky.speak
+# fido = GoodDog.new("Fido")
+# # puts fido
+# puts fido.speak
 
-sparky.name = "spartacus"
+# puts sparky.name
 
-puts sparky.name
+# sparky.name = "spartacus"
+
+# puts sparky.name
 
 
